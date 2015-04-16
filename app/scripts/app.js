0a05b47a4cdf1b8c15264c380b842b5ac3e49cd5
adamwall <nagasee9@gmail.com>
2015-04-16 07:31:46 -0500

    removed default template components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,33 +30,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'AppCtrl'
       })
 
-      .state('app.search', {
-        url: "/search",
-        views: {
-          'menuContent': {
-            templateUrl: "templates/search.html"
-          }
-        }
-      })
-
-      .state('app.browse', {
-        url: "/browse",
-        views: {
-          'menuContent': {
-            templateUrl: "templates/browse.html"
-          }
-        }
-      })
-      .state('app.playlists', {
-        url: "/playlists",
-        views: {
-          'menuContent': {
-            templateUrl: "templates/playlists.html",
-            controller: 'PlaylistsCtrl'
-          }
-        }
-      })
-
       .state('app.stops', {
         url: "/stops",
         views: {
@@ -95,17 +68,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             controller: 'StopsCtrl'
           }
         }
-      })
-
-      .state('app.single', {
-        url: "/playlists/:playlistId",
-        views: {
-          'menuContent': {
-            templateUrl: "templates/playlist.html",
-            controller: 'PlaylistCtrl'
-          }
-        }
       });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/favorites');
   });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,49 +1,5 @@
 angular.module('starter.controllers', ['ngCordova'])
 
-  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-    // Form data for the login modal
-    $scope.loginData = {};
-
-    // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-      scope: $scope
-    }).then(function(modal) {
-      $scope.modal = modal;
-    });
-
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function() {
-      $scope.modal.hide();
-    };
-
-    // Open the login modal
-    $scope.login = function() {
-      $scope.modal.show();
-    };
-
-    // Perform the login action when the user submits the login form
-    $scope.doLogin = function() {
-      console.log('Doing login', $scope.loginData);
-
-      // Simulate a login delay. Remove this and replace with your login
-      // code if using a login system
-      $timeout(function() {
-        $scope.closeLogin();
-      }, 1000);
-    };
-  })
-
-  .controller('PlaylistsCtrl', function($scope) {
-    $scope.playlists = [
-      { title: 'R', id: 1 },
-      { title: 'Chill', id: 2 },
-      { title: 'Dubstep', id: 3 },
-      { title: 'Indie', id: 4 },
-      { title: 'Rap', id: 5 },
-      { title: 'Cowbell', id: 6 }
-    ];
-  })
-
   .controller('StopsCtrl', function($scope, $http, $cordovaFile) {
     $scope.search = function(query){
       $http.get('http://www.cumtd.com/autocomplete/Stops/v1.0/json/search?query=' + query).success(function (data){
@@ -216,6 +172,3 @@ angular.module('starter.controllers', ['ngCordova'])
     });
   })
 //-------------------STOP CONTROLLER END---------------------------------------------------------------
-
-  .controller('PlaylistCtrl', function($scope, $stateParams) {
-  });
